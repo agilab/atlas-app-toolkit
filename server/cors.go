82f6c3c2a58d9d1cb8b66278616304839ab11f0c
molon <dudl@qq.com>
2019-01-31 21:04:09 +0800

    优化cors

--- server/cors.go
+++ server/cors.go
@@ -11,23 +11,27 @@ var (
 
 func allowCORS(h http.Handler, allowOrigins []string, extraAllowHeaders []string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			if len(allowOrigins) > 0 {
-				for _, allowOrigin := range allowOrigins {
-					if origin == allowOrigin {
-						w.Header().Set("Access-Control-Allow-Origin", origin)
-						break
-					}
-				}
-			} else {
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-			}
-
-			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
-				preflightHandler(w, r, extraAllowHeaders)
-				return
-			}
+		origin := r.Header.Get("Origin")
+		if origin == "" {
+			origin = r.URL.Scheme + "//" + r.URL.Host
 		}
+
+		if len(allowOrigins) > 0 {
+			for _, allowOrigin := range allowOrigins {
+				if origin == allowOrigin {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+			preflightHandler(w, r, extraAllowHeaders)
+			return
+		}
+
 		h.ServeHTTP(w, r)
 	})
 }
